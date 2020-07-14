aa2aeab6ccaf6f23f842d6c0043516906822251e
Mikołaj Przybylski <clovers02123@gmail.com>
2020-07-14 12:29:45 +0200

    Added boundaries and script to resize camera based on screen size

---
+++ Hailon/Assets/Scripts/CameraResize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResize : MonoBehaviour
+{
+    public SpriteRenderer background;
+
+    private void Awake()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = background.bounds.size.x / background.bounds.size.y;
+
+        if (screenRatio >= targetRatio)
+            GetComponent<Camera>().orthographicSize = background.bounds.size.y * 0.5f;
+
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            GetComponent<Camera>().orthographicSize = background.bounds.size.y * 0.5f * differenceInSize;
+        }
+    }
+}
