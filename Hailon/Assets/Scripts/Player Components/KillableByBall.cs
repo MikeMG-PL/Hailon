c29b6598590007bed3f4579ac38d99f6870d67d8
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-07-18 14:56:31 +0200

    Player is KILLED when hit by the ball

---
+++ Hailon/Assets/Scripts/Player Components/KillableByBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillableByBall : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("LEVEL FALIED!");
+            FailLevel();
+        }
+    }
+
+    void FailLevel()
+    {
+        Destroy(gameObject);
+    }
+}
