6a178745b589a0b1e392816d47f70373ac6e0b49
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-07-24 20:57:41 +0200

    New dragging

--- Hailon/Assets/Scripts/Player Components/Controlable.cs
+++ Hailon/Assets/Scripts/Player Components/Controlable.cs
@@ -9,25 +9,37 @@ public class Controlable : MonoBehaviour
     Touch touch;
     RaycastHit2D hit;
     bool touchStarted;
+    GameObject sample;
 
     void Update()
     {
         TouchSet();
+        Sliding();
+        ProperXPOS();
+    }
 
+    void Sliding()
+    {
         if (hit.collider == GetComponent<CapsuleCollider2D>())
         {
             if (touch.phase == TouchPhase.Began)
+            {
+                sample = transform.GetChild(0).gameObject;
+                sample = Instantiate(sample, new Vector2(touchPosition.x, transform.position.y), Quaternion.identity);
+                transform.SetParent(sample.transform);
                 touchStarted = true;
+            }
 
             if (touchStarted == true)
-                transform.position = new Vector2(touchPosition.x, transform.position.y);
+                sample.transform.position = new Vector2(touchPosition.x, transform.position.y);
         }
 
         if (touch.phase == TouchPhase.Ended)
+        {
+            transform.SetParent(null);
             touchStarted = false;
-
-        //ProperYPOS();
-        ProperXPOS();
+            Destroy(sample);
+        }
     }
 
     void ProperXPOS()
@@ -38,12 +50,6 @@ public class Controlable : MonoBehaviour
         if (transform.position.x < -2.2f)
             transform.position = new Vector2(-2.2f, -4.35f);
     }
-
-    /*void ProperYPOS()
-    {
-        Vector2 position = new Vector2(transform.position.x, -4.35f);
-        transform.position = position;
-    }*/
 
     void TouchSet()
     {
