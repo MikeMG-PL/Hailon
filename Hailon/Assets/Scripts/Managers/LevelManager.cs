4a83b06371fafd3c1223a869775f36652ee530c0
Mikołaj Przybylski <clovers02123@gmail.com>
2020-07-30 22:44:19 +0200

    Small fix in assigning data after finishing level

--- Hailon/Assets/Scripts/Managers/LevelManager.cs
+++ Hailon/Assets/Scripts/Managers/LevelManager.cs
@@ -11,7 +11,7 @@ public class LevelManager : MonoBehaviour
     public GameObject levelFinishedUI;
     public Objective objective;
 
-    private void Start()
+    void Start()
     {
         if (objective == null) Debug.LogError("You need to assign Finish object (object with objective script)");
 
@@ -20,13 +20,13 @@ public class LevelManager : MonoBehaviour
         objective.ObjectiveReached += OnObjectiveReached;
     }
 
-    private void OnObjectiveReached(object sender, System.EventArgs e)
+    void OnObjectiveReached(object sender, System.EventArgs e)
     {
         levelData.finished = true;
         levelFinishedUI.SetActive(true);
 
         PlayerData playerData = GameManager.gameManager.GetComponent<PlayerData>();
-        if (playerData.level < levelData.index) playerData.level = levelData.index;
+        if (playerData.level == levelData.index) playerData.level = levelData.index + 1;
         SaveManager.SaveProgress(playerData);
     }
 }
