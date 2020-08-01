5fc95ed9446720e02a0b72dc59b55e1b47038ab1
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-08-01 21:08:03 +0200

    target framerate

--- Hailon/Assets/Scripts/Managers/GameManager.cs
+++ Hailon/Assets/Scripts/Managers/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
+        Application.targetFrameRate = 60;
+
         if (!gameStart)
         {
             gameManager = this;
