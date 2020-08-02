14f3b88303f447cb1a07d15455bc41c7bd948532
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-08-02 16:40:31 +0200

    Exit fix

--- Hailon/Assets/Scripts/Managers/ExitManager.cs
+++ Hailon/Assets/Scripts/Managers/ExitManager.cs
@@ -5,16 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ExitManager : MonoBehaviour
 {
+    public ButtonManager buttonManager;
+
     void Update()
     {
         if (SceneManager.GetActiveScene().name != "Menu" && SceneManager.GetActiveScene().name != "Loading")
-        {
+        {       
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                GameManager.gameManager.LoadScene("Menu", SceneManager.GetActiveScene().name);
+                //GameManager.gameManager.LoadScene("Menu", SceneManager.GetActiveScene().name);
+                buttonManager.Pause();
             }
         }
-        else if (Input.GetKeyDown(KeyCode.Escape))
+
+        if (SceneManager.GetActiveScene().name == "Menu" && Input.GetKeyDown(KeyCode.Escape))
             Application.Quit(); // dodać do notifications to, że gracz jest w LevelMenu i żeby przycisk escape także cofał do menu a nie zamykał apkę!
     }
 }
